aae6ab7812633de2698f87b43d820972e4276bfe
bsid24082 <118895012+bsid24082@users.noreply.github.com>
2024-02-04 16:50:57 +0530

    Final code

---
+++ script.js
@@ -0,0 +1,18 @@
+const imageContaninerE1 = document.querySelector(".image-container")
+
+const btnE1 = document.querySelector(".btn")
+
+btnE1.addEventListener("click", () => {
+    imageNum = 12
+    addNewImages()
+});
+
+function addNewImages(){
+for (let index = 0; index < imageNum; index++) {
+    const newImgE1 = document.createElement('img')
+    newImgE1.src= `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
+    imageContaninerE1.appendChild(newImgE1)
+}
+}
+
+
